ac4748b330e2212efd17a8a06fb44236bbf6d20e
Guido van Duijvenvoorde <72505125+Guido2002@users.noreply.github.com>
2023-11-25 11:57:26 +0100

    fixing bug switching targets

--- Assets/PhoneScript.cs
+++ Assets/PhoneScript.cs
@@ -1,17 +1,23 @@
+using Fusion.XR.Shared.Grabbing.NetworkHandColliderBased;
 using UnityEngine;
 
 public class PhoneScript : MonoBehaviour
 {
     public AudioClip ringSound; // The initial sound to play when the phone rings
     public AudioClip newClip; // The new sound to play after collision
+    public SpotlightController spotlightController;
 
+    private NetworkHandColliderGrabbable grabbable;
     private AudioSource audioSource;
     private bool isRinging;
     private bool isPlayingNewClip;
+    private bool hasSwitchedTarget; // Flag to keep track of target switch
 
     private void Start()
     {
+        grabbable = GetComponent<NetworkHandColliderGrabbable>();
         InitializeAudioSource();
+        hasSwitchedTarget = false; // Initialize the flag
     }
 
     private void InitializeAudioSource()
@@ -60,8 +66,21 @@ public class PhoneScript : MonoBehaviour
     {
         // Stop the current clip and set the new one
         audioSource.Stop();
-        audioSource.loop = true;
+        audioSource.loop = false;
         audioSource.PlayOneShot(newClip);
+
+        if (!hasSwitchedTarget)
+        {
+            Invoke("SwitchNextTarget", newClip.length);
+        }
     }
 
+    private void SwitchNextTarget()
+    {
+        if (grabbable != null)
+        {
+            spotlightController.SwitchToNextTarget();
+            hasSwitchedTarget = true; // Update the flag after switching target
+        }
+    }
 }
