eee8d1c1bf26126a0973fae4ec29e599874ba9f1
Yeic554 <71875612+Yeic554@users.noreply.github.com>
2023-12-17 12:44:07 +0100

    Gordijn manager
    Manager voor gordijnen toegevoegd

--- Assets/Gordijn_Dicht.cs
+++ Assets/Gordijn_Dicht.cs
@@ -6,6 +6,7 @@ public class Gordijn_Dicht : MonoBehaviour
 {
     public GameObject gordijnLinks;
     public GameObject gordijnRechts;
+    private bool isDicht = false;
 
     private Vector3 newscale;
 
@@ -15,7 +16,18 @@ public class Gordijn_Dicht : MonoBehaviour
         newscale = new Vector3(105, 100, 100);
         gordijnLinks.transform.localScale = newscale;
         gordijnRechts.transform.localScale = newscale;
+        Interact();
     }
 
+    private void Interact()
+    {
+        isDicht = true;
+        CandleManager.Instance.CheckAllCandlesLit();
+    }
+
+    public bool IsDicht()
+    {
+        return isDicht;
+    }
 
 }
